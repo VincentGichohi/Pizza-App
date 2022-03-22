2e20f14f7ecd43feb29492a985f5dd337fab73e4
VincentGichohi <vincentegichohi@gmail.com>
2022-03-22 07:35:53 +0300

    Added a constructor for the pizza

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@
   
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.querySelectorAll('.needs-validation')
-  
+
     // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
       .forEach(function (form) {
@@ -19,14 +19,34 @@
       })
   })()
 
-//a constructor for the pizza size and the price
-function Pizza() {
-    this.smallPrice = 800;
-    this.mediumPrice = 1000;
-    this.largePrice = 1200;
-    this.toppings = 400;
-    this.crust = 300;
-    this.smallTotal = function() {
-        return parseInt(smallPrice + toppings + crust);
-    }
-};
+//   var username = document.getElementById("username");
+//   var pass = document.getElementById("pass");
+//   pass.onfocus = function(){
+//   if(pass.value=="ENTER PASSWORD"){
+//   pass.value="";
+//   }
+//   };
+//   username.onfocus = function(){
+//   if(this.value=="ENTER USERNAME"){
+//   this.value="";
+//   }
+//   };
+//   pass.onblur = function(){
+//   if(this.value==""){
+//   pass.value="ENTER PASSWORD";
+//   }
+//   };
+//   username.onblur = function(){
+//   if(this.value==""){
+//   this.value="ENTER USERNAME";
+//   }
+//   };
+
+var pizza = ['small', 'medium', 'large']
+
+//a constructor that saves the flavour of pizza, the size and the price
+function PizzaDetails(pizzaFlavour, pizzaSize, pizzaPrice) {
+    this.pizzaFlavour = pizzaFlavour;
+    this.pizzaSize  = pizzaSize;
+    this.pizzaPrice = pizzaPrice;
+}
